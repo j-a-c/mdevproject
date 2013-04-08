7a4ba6c9620051c82158992897fc6e4e4007bc1e
Joshua A. Campbell <runguy@ymail.com>
2013-04-08 13:06:07 -0400

    student activity listeners

--- src/com/joshuac/campusconnect/LoginActivity.java
+++ src/com/joshuac/campusconnect/LoginActivity.java
@@ -32,9 +32,20 @@ public class LoginActivity extends Activity
 		EditText usernameText = (EditText) findViewById(R.id.usernameText);
 		EditText passwordText = (EditText) findViewById(R.id.passwordText);
 		
-		//set up intent
-		//Intent intent = new Intent(getBaseContext(), AdminActivity.class);
-		Intent intent = new Intent(getBaseContext(), MapActivity.class);
+		Intent intent;
+		//set up intent - just used for testing
+		if(usernameText.equals("admin"))
+		{
+			intent = new Intent(getBaseContext(), AdminActivity.class);
+		}
+		else if(usernameText.equals("student"))
+		{
+			intent = new Intent(getBaseContext(), StudentActivity.class);
+		}
+		else
+			intent = new Intent(getBaseContext(), MapActivity.class);
+		
+		//put data into intent
 		intent.putExtra("username", usernameText.getText().toString());
 		startActivity(intent);
 	}
--- src/com/joshuac/campusconnect/StudentActivity.java
+++ src/com/joshuac/campusconnect/StudentActivity.java
@@ -3,6 +3,7 @@ package com.joshuac.campusconnect;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudentActivity extends Activity
 {
@@ -22,4 +23,37 @@ public class StudentActivity extends Activity
 
 		
 	}//end onCreate
+	
+	
+	
+	//called when "view upcoming events" button is pressed
+	public void viewUpcomingEvents(View v)
+	{
+		System.out.println("up");
+	}
+	
+	//called when "current events in area" button is pressed
+	public void viewEventsInArea(View v)
+	{
+		System.out.println("area");
+	}
+	
+	//called when "checkin" button is pressed
+	public void checkinToEvent(View v)
+	{
+		System.out.println("in");
+	}
+	
+	//called when "friends" button is pressed
+	public void viewFriendActivity(View v)
+	{
+		System.out.println("friend");
+	}
+	
+	//called when "setting" button is pressed
+	public void viewSettings(View v)
+	{
+		System.out.println("settings");
+	}
+	
 }//end StudentActivity
